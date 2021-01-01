4ce1026faad470fa4e8f61638b6da0ea23616832
GitHub Action <action@github.com>
2021-01-01 06:57:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/cxksliling/forfun/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/cxksliling/forfun/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.cxksliling.forfun.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.cxksliling.forfun.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/cxksliling/forfun/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/cxksliling/forfun/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.cxksliling.forfun.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cxksliling.forfun.MyBundle
 
 class MyApplicationService {
 
